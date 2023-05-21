ae01555f8641bf9e5afbd6400451db50e1750b59
Kamaal <kamaal.f1@gmail.com>
2023-05-21 13:48:20 +0000

    Adding update methods

--- webdav/contacts/views.py
+++ webdav/contacts/views.py
@@ -1,11 +1,15 @@
 from rest_framework import viewsets, mixins
+from rest_framework.response import Response
 
 from webdav.contacts.models import Contact
 from webdav.contacts.serializers import ContactSerializer
 
 
 class ContactsViewSet(
-    mixins.CreateModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    viewsets.GenericViewSet,
 ):
     queryset = Contact.objects.all().order_by("id")
     serializer_class = ContactSerializer
@@ -13,3 +17,11 @@ class ContactsViewSet(
     def perform_create(self, serializer):
         serializer.validated_data.setdefault("user", self.request.user)
         return super().perform_create(serializer)
+
+    def options(self, request, *args, **kwargs):
+        response = super().options(request, *args, **kwargs)
+        return Response(
+            data=response.data,
+            status=200,
+            headers={**response.headers, "Allows": "OPTIONS GET HEAD POST DELETE"},  # type: ignore
+        )
